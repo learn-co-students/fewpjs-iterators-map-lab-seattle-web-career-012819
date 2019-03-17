edaeab0dc0bda5c9241c0bde6e3fee6fe4303e31
github username <crishanks12@gmail.com>
2019-03-16 21:17:01 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function titleCased(tutorials) {
+  const titlizedArray = tutorials.map((sentence) => {
+    sentenceArray = sentence.split(' ');
+    let titlizedSentence = '';
+    for (let i = 0; i < sentenceArray.length; i++) {
+      const currentWord = sentenceArray[i];
+      const firstLetter = currentWord[0].toUpperCase();
+      const rest = currentWord.slice(1);
+      titlizedSentence += `${firstLetter + rest} `;
+    }
+    return titlizedSentence.trim();
+  });
+  return titlizedArray;
 }
